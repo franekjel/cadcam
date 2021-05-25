94eb1d7b3962010f96ead01188a88f28e62c0080
Franciszek Jełowicki <franek.jelowicki@gmail.com>
2021-05-25 21:14:53 +0200

    working translation

--- src/ICP.cpp
+++ src/ICP.cpp
@@ -1,6 +1,9 @@
 #include "ICP.h"
 
 #include <cstdio>
+#include <fstream>
+
+#include <Eigen/Geometry>
 
 #include <BRepExtrema_DistShapeShape.hxx>
 #include <BRep_Builder.hxx>
@@ -17,14 +20,17 @@
 
 std::vector<std::pair<TopoDS_Vertex, TopoDS_Vertex>> GetVertexPairList(const std::vector<TopoDS_Shape>& points) {
     std::vector<std::pair<TopoDS_Vertex, TopoDS_Vertex>> edges;
-    for(const auto& point : points) {
+    for (const auto& point : points) {
         TopoDS_Vertex vv;
         int i = 0;
         for (TopExp_Explorer vertexExplorer(point, TopAbs_VERTEX); vertexExplorer.More(); vertexExplorer.Next(), i++) {
-            const auto &vertex = TopoDS::Vertex(vertexExplorer.Current());
-            if (vertex.IsNull())continue;
-            if(i % 2 == 0) vv = vertex;
-            else edges.emplace_back(vv, vertex);
+            const auto& vertex = TopoDS::Vertex(vertexExplorer.Current());
+            if (vertex.IsNull())
+                continue;
+            if (i % 2 == 0)
+                vv = vertex;
+            else
+                edges.emplace_back(vv, vertex);
         }
     }
     return edges;
@@ -72,21 +78,33 @@ std::tuple<Standard_Real, gp_Vec, gp_Pnt> DistAndNormal(const TopoDS_Shape& mode
     //to się nie powinno stać
 }
 
-Eigen::Matrix<double, 6, 1> Gradient(const TopoDS_Shape& model, const std::pair<TopoDS_Vertex, TopoDS_Vertex>& point, const Eigen::Matrix3d& rotation, const Eigen::Matrix3d& translation, const float alpha) {
+Eigen::Matrix<double, 6, 1> Gradient(const TopoDS_Shape& model, const std::pair<TopoDS_Vertex, TopoDS_Vertex>& point, const Eigen::Matrix4d& rotation, const Eigen::Matrix4d& translation, const float alpha) {
+
+    /*
+    v,n - punkt i normalna po transformacji jako eigenowy wektor
+    VV,NN - punkt i normalna po transformacji w formacie cascadowym jako wejście do funkcji dystansu
+    V,N - wyjście funkcji dystansu w formacie cascadowym
+    */
+
     Eigen::Matrix<double, 6, 1> re;
     gp_Pnt p = BRep_Tool::Pnt(point.first);
-    Eigen::Vector3d v = Eigen::Vector3d(p.X(), p.Y(), p.Z());
+    Eigen::Vector4d v = Eigen::Vector4d(p.X(), p.Y(), p.Z(), 1.0f);
     v = translation * rotation * v;
-    //v[0] += 10.0; //TEST
-    gp_Vec NN = gp_Vec(BRep_Tool::Pnt(point.first), BRep_Tool::Pnt(point.second));
-    NN.Normalize();
+    v /= v.w();
+
+    gp_Pnt p2 = BRep_Tool::Pnt(point.second);
+    Eigen::Vector4d n = Eigen::Vector4d(p2.X(), p2.Y(), p2.Z(), 0.0f);
+    n = rotation * n;
+
+    //Eigen::Vector3d n = Eigen::Vector3d(v2.x() - v.x(), v2.y() - v.y(), v2.z() - v.z());
+    //n.normalize();
 
     BRep_Builder builder;
-    TopoDS_Vertex vert;
-    builder.MakeVertex(vert, gp_Pnt(v.x(), v.y(), v.z()), 0.01f);
-    auto [dist, N, V] = DistAndNormal(model, vert, NN);
-    p = BRep_Tool::Pnt(point.second);
-    Eigen::Vector3d n = Eigen::Vector3d(p.X() - v.x(), p.Y() - v.y(), p.Z() - v.z());
+    TopoDS_Vertex VV;
+    builder.MakeVertex(VV, gp_Pnt(v.x(), v.y(), v.z()), 0.01f);
+    gp_Vec NN = gp_Vec(n.x(), n.y(), n.z());
+    NN.Normalize();
+    auto [dist, N, V] = DistAndNormal(model, VV, NN);
     re(0) = 2 * alpha * dist * N.X();
     re(1) = 2 * alpha * dist * N.Y();
     re(2) = 2 * alpha * dist * N.Z();
@@ -100,8 +118,8 @@ std::pair<Eigen::Matrix3Xf, Eigen::Matrix3Xf> ICP(const std::vector<TopoDS_Shape
     const int N = 400;
     const float alpha = 0.8f;
 
-    Eigen::Matrix3d rotation = Eigen::Matrix3d::Identity();
-    Eigen::Matrix3d translation = Eigen::Matrix3d::Identity();
+    Eigen::Matrix4d rotation = Eigen::Matrix4d::Identity();
+    Eigen::Matrix4d translation = Eigen::Matrix4d::Identity();
 
     std::vector<std::pair<TopoDS_Vertex, TopoDS_Vertex>> edges = GetVertexPairList(points);
     TopoDS_Compound compound;
@@ -110,31 +128,44 @@ std::pair<Eigen::Matrix3Xf, Eigen::Matrix3Xf> ICP(const std::vector<TopoDS_Shape
     for (int i = 0; i < model.size(); ++i)
         builder.Add(compound, model[i]);
 
-    /*for (const auto& e : edges) {
-        const auto& p1 = BRep_Tool::Pnt(e.first);
-        const auto& p2 = BRep_Tool::Pnt(e.second);
-        printf("(%lf, %lf, %lf)-(%lf, %lf, %lf)\n", p1.X(), p1.Y(), p1.Z(), p2.X(), p2.Y(), p2.Z());
-    }*/
-    Eigen::Matrix<double, 6, 1> g = Eigen::Matrix<double, 6, 1>::Zero(6, 1);
-    for (int i = 0; i < edges.size(); i++) {
-        g += Gradient(compound, edges[i], rotation, translation, 0.5f) / points.size();
-    }
-    double a = sqrt(g[3] * g[3] + g[4] * g[4] + g[5] * g[5]);
+    std::ofstream file("matrices.txt", std::ios::trunc);
+    for (int i = 0; i < 100; i++) {
 
-    double s = abs(g[0]) + abs(g[1]) + abs(g[2]);
-    if (s > 0.001f) {
-        g[0] /= s;
-        g[1] /= s;
-        g[2] /= s;
+        Eigen::Matrix<double, 6, 1> g = Eigen::Matrix<double, 6, 1>::Zero(6, 1);
+        for (int i = 0; i < edges.size(); i++) {
+            g += Gradient(compound, edges[i], rotation, translation, 1.0f) / points.size();
+        }
+        g /= -double(edges.size());
+
+        Eigen::Transform<double, 3, Eigen::Affine> transformation;
+        transformation = Eigen::Translation<double, 3>(g[0], g[1], g[2]);
+        translation *= transformation.matrix();
+        auto v = translation.col(3);
+        file << v.x() << " " << v.y() << " " << v.z();
+
+        double s = abs(g[0]) + abs(g[1]) + abs(g[2]);
+        s = abs(g[3]) + abs(g[4]) + abs(g[5]);
+        if (s > 0.001f) {
+            g[3] /= s;
+            g[4] /= s;
+            g[5] /= s;
+        }
+
+        file << " 1 0 0 0" << std::endl;
+
+        /*
+        Trzeba zapisywać do pliku w formacie
+        Tx Ty Tz Rx Ry Rz alfa
+        Gdzie T to translacja
+        R - oś obrotu
+        alfa - kąt
+        file << ...
+        */
+
+        transformation = Eigen::Transform<double, 3, Eigen::Affine>::Identity();
+
+        printf("%lf %lf %lf %lf %lf %lf\n", g[0], g[1], g[2], g[3], g[4], g[5]);
     }
-    s = abs(g[3]) + abs(g[4]) + abs(g[5]);
-    if (s > 0.001f) {
-        g[3] /= s;
-        g[4] /= s;
-        g[5] /= s;
-    }
-    printf("%lf %lf %lf %lf %lf %lf\n", g[0], g[1], g[2], g[3], g[4], g[5]);
-    printf("%lf\n", a);
 
     return {};
 }
